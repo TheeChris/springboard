5038363722650f5f87267baa5712c8b59d427394
Chris Lynch <echrislynch@gmail.com>
2018-12-04 22:05:48 -0800

    add cookie-cutter template

---
+++ Capstone_2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting hospital readmission using NLP and unstructured clinical notes',
+    author='E. Chris Lynch',
+    license='MIT',
+)
